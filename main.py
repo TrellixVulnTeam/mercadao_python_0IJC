bc04d68a317e1e76f1d2ab989cda44565d8a50c7
Leonardo Ramos <leonardo.meinerzramos28@gmail.com>
2018-10-11 09:06:09 -0300

    teste da função do radiogroup

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# classe principal
+from Interface import Interface as GUI
+
+tela = GUI(800,500)
+
+
+
+a = tela.radio_group(20,20,["teste", "lalala", "nfsanfiwadilsan", "teste", "lalala", "nfsanfiwadilsan"])
+
+
+
+
+
+tela.start()
