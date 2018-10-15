90fea97032033935ef941011d8e3303ecdc2b368
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-10-15 13:49:08 -0300

    adicionado o formulario de cadastro de produto

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from Interface import Interface as GUI
 
-tela = GUI(800,500)
+tela = GUI(800, 500)
 
+# titulo da pagina
+tela.label(30, 20, "Mercadão Python", font=("Arial", 20))
 
+tela.label(30, 60, "Cadastro de produtos", font=("Arial", 12))
 
-a = tela.radio_group(20,20,["teste", "lalala", "nfsanfiwadilsan", "teste", "lalala", "nfsanfiwadilsan"])
+tela.label(30, 100, "Codigo: ")
+codigo = tela.entry(200, 100, 30)
 
+tela.label(30, 140, "Descrição: ")
+descricao = tela.entry(200, 140, 30)
 
+tela.label(30, 180, "Preço unitário: ")
+preco = tela.entry(200, 180, 30)
 
+tela.label(30, 220, "Unidade de medida")
+radio_unidade = tela.radio_group(200, 200, ["Un", "Kg", "M", "PC"])
 
-
+tela.button(280, 320, "Cadastrar produto")
 tela.start()
