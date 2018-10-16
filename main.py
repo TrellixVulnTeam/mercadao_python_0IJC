65c435d61836e1f1d8c05f8234c22f4347cdabfe
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-10-16 14:50:45 -0300

    nomes de variaveis renomeadas para não causar conflito com outro formulario. Adicionado novo formulario para o cadastro de clientes

--- main.py
+++ main.py
@@ -8,16 +8,33 @@ tela.label(30, 20, "Mercadão Python", font=("Arial", 20))
 tela.label(30, 60, "Cadastro de produtos", font=("Arial", 12))
 
 tela.label(30, 100, "Codigo: ")
-codigo = tela.entry(200, 100, 30)
+codigo_produto = tela.entry(200, 100, 30)
 
 tela.label(30, 140, "Descrição: ")
-descricao = tela.entry(200, 140, 30)
+descricao_produto = tela.entry(200, 140, 30)
 
 tela.label(30, 180, "Preço unitário: ")
-preco = tela.entry(200, 180, 30)
+preco_produto = tela.entry(200, 180, 30)
 
-tela.label(30, 220, "Unidade de medida")
-radio_unidade = tela.radio_group(200, 200, ["Un", "Kg", "M", "PC"])
+tela.label(30, 220, "Unidade de medida: ")
+radio_unidade_produto = tela.radio_group(200, 200, ["Un", "Kg", "M", "PC"])
 
 tela.button(280, 320, "Cadastrar produto")
+
+# cadastro de clientes
+tela.label(450, 60, "Cadastro de clientes", font=("Arial", 12))
+
+tela.label(450, 100, "Nome: ")
+nome_cliente = tela.entry(580, 100, 30)
+
+tela.label(450, 140, "CPF: ")
+cpf_cliente = tela.entry(580, 140, 30)
+
+tela.label(450, 180, "email: ")
+email_cliente = tela.entry(580, 180, 30)
+
+tela.label(450, 220, "telefone: ")
+telefone_cliente = tela.entry(580, 220, 30)
+
+tela.button(660, 320, "Cadastrar cliente")
 tela.start()
